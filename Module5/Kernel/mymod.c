48c1e846230988df215a4070d4067b7d5705be6f
Dipika Pandey <dipika.pandey@wct-inc.com>
2017-02-17 15:08:18 -0500

    Fixed printDMABuf printing issue

--- Module5/Kernel/mymod.c
+++ Module5/Kernel/mymod.c
@@ -309,15 +309,10 @@ void drainDMA(int count){
    sync_kick_fifo(); 
 }
 
-void printDMABuf(void){
-    int i = 0;
-    while(i < 3)
-    {
-        printk(KERN_ALERT "[KERNEL] dma_buf -> k_base addr: %lx \n", *(dma_buf[i].k_base));
-        printk(KERN_ALERT "[KERNEL] dma_buf -> p_base addr: %lx \n", (unsigned long)dma_buf[i].p_base);
-        printk(KERN_ALERT "[KERNEL] dma_buf -> u_base addr: %lx \n", dma_buf[i].u_base);
-        ++i;
-    }
+void printDMABuf(int i){
+  printk(KERN_ALERT "[KERNEL] dma_buf -> k_base addr: %lx \n", *(dma_buf[i].k_base));
+  printk(KERN_ALERT "[KERNEL] dma_buf -> p_base addr: %lx \n", (unsigned long)dma_buf[i].p_base);
+  printk(KERN_ALERT "[KERNEL] dma_buf -> u_base addr: %lx \n", dma_buf[i].u_base);
 }
 
 irqreturn_t dma_intr(int irq, void *dev_id, struct pt_regs *regs)
@@ -376,13 +371,13 @@ long kyouko3_ioctl(struct file *fp, unsigned int cmd, unsigned long arg)
       case BIND_DMA:
           for(i = 0; i < NUM_DMA_BUF; ++i)
           {
-              printDMABuf();
+              //printDMABuf();
               dma_buf[i].k_base = pci_alloc_consistent(kyouko3.kyouko3_pci_dev, DMA_BUF_SIZE, &dma_buf[i].p_base);
-              printDMABuf();
+              printDMABuf(i);
               kyouko3.curr_dma_mmap_index = i;
               dma_buf[i].u_base = vm_mmap(fp, 0, DMA_BUF_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, 0x1);
               //dma_buf[i].u_base = vm_mmap(fp, dma_buf[i].p_base, DMA_BUF_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, 0x1);
-              printDMABuf();
+              printDMABuf(i);
               //TODO: Handle the failure cases of above calls
           }
           *(unsigned long*)arg = dma_buf[0].u_base;
